abdf571b4e335b30d40fdc686b6c1a55279f24f8
msouff <17328135+msouff@users.noreply.github.com>
2021-04-06 02:14:49 -0600

    use branch that triggered event

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Downloading app repository."
 cd / && /opt/conda/envs/tethys/bin/git clone https://github.com/${1}.git
 
 echo "Setting repository branch."
-cd /$(basename $1) && /opt/conda/envs/tethys/bin/git checkout $2
+cd /$(basename $1) && /opt/conda/envs/tethys/bin/git checkout $(basename $2)
 
 echo "Running checks."
 /opt/conda/envs/tethys/bin/python /tethys_app_linter.py $1
