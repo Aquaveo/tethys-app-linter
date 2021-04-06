1085a749d2656e4f0d13fdb40f5a360085ffd855
Michael Souffront <17328135+msouff@users.noreply.github.com>
2021-04-06 01:15:00 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-/opt/conda/envs/tethys/bin/git clone https://github.com/${1}.git
+cd / && /opt/conda/envs/tethys/bin/git clone https://github.com/${1}.git
 
 /opt/conda/envs/tethys/bin/python /tethys_app_linter.py $1
