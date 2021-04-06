546e92a5cc35ef58e438385da09ce0578b4c4738
msouff <17328135+msouff@users.noreply.github.com>
2021-04-06 01:48:23 -0600

    use branch that triggered event

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 cd / && /opt/conda/envs/tethys/bin/git clone https://github.com/${1}.git
+cd $(basename $1) && /opt/conda/envs/tethys/bin/git checkout $2
 
 /opt/conda/envs/tethys/bin/python /tethys_app_linter.py $1
