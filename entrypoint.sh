ed08c4c21688110dd702a1f46a08b3a871636a16
msouff <17328135+msouff@users.noreply.github.com>
2021-04-07 11:23:45 -0600

    added flake8

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,6 @@
 
 echo "Running checks."
 /opt/conda/envs/tethys/bin/python /tethys_app_linter.py "$1" "$GITHUB_WORKSPACE"
+
+echo "Running flake8"
+/opt/conda/envs/tethys/bin/flake8 "$GITHUB_WORKSPACE"
